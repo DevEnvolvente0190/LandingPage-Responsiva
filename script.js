d47013d268e619c64468aee6e9385937fff06baa
DevEnvolvente0190 <97799788+DevEnvolvente0190@users.noreply.github.com>
2022-07-28 08:31:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
